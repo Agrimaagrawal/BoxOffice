47bb79bf710b8aec2629b1a079fa1572a4485681
Agrima Agrawal <84567933+Agrimaagrawal@users.noreply.github.com>
2023-07-06 23:25:04 +0530

    Update ShowCard.jsx

--- src/components/Showfol/ShowCard.jsx
+++ src/components/Showfol/ShowCard.jsx
@@ -8,7 +8,7 @@ const ShowCard = ({name,image,id,summary,onStarclick,isStarred}) => {
       <h1 className='font-extrabold text-2xl p-3'>{name}</h1>
       <p>{SummaryStripped}</p>
       <div>
-        <a href={`/show/${id}` } className='p-4 text-blue-700 text-lg font-bold  underline' target='_blank' >Read more</a>
+        <a href={`/show/${id}` } className='p-4 text-blue-700 text-lg font-bold  underline'  >Read more</a>
         <button type="button" className='' onClick={()=>onStarclick(id)}>{isStarred ? <img src="star.png" className="w-10 "></img>:<img src="unstar.png " className="w-10"></img>}</button>
       </div>
       
